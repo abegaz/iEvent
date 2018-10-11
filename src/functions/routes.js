0aca53da8f4b8b09fb88591dfdeb25fcb325f635
QuintinKerns <qakern8560@ung.edu>
2018-10-10 23:56:41 -0400

    Added new routes

--- src/functions/routes.js
+++ src/functions/routes.js
@@ -13,4 +13,69 @@ Router.get('/', (req, res) => {
     // This includes the username, url to the profile pic in storage, etc.
 });
 
+// Event Page Route
+Router.get('/events', (req, res) => {
+    // How long the page is cached in Google's CDNs
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+
+    // Renders the ejs page into html, which is sent to the client
+    res.render('events', {});
+
+    // Inside the brackets above, add any data in json format needed in the ejs file.
+    // This includes the username, url to the profile pic in storage, etc.
+});
+
+// Event Page Route
+Router.get('/events/:id', (req, res) => {
+    // How long the page is cached in Google's CDNs
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+
+    // Renders the ejs page into html, which is sent to the client
+    res.render('index', {});
+
+    // Inside the brackets above, add any data in json format needed in the ejs file.
+    // This includes the username, url to the profile pic in storage, etc.
+});
+
+// Edit Event Page Route
+Router.get('/events/:id/edit', (req, res) => {
+    // How long the page is cached in Google's CDNs
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+
+    // Renders the ejs page into html, which is sent to the client
+    res.render('event-edit', {});
+
+    // Inside the brackets above, add any data in json format needed in the ejs file.
+    // This includes the username, url to the profile pic in storage, etc.
+});
+
+// Edit Event Put Route
+Router.put('/events/:id/edit', (req, res) => {
+    // Edit event in the database from the form response data in req.body
+    
+});
+
+// New Event Page Route
+Router.get('/events/new', (req, res) => {
+    // How long the page is cached in Google's CDNs
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+
+    // Renders the ejs page into html, which is sent to the client
+    res.render('event-new', {});
+
+    // Inside the brackets above, add any data in json format needed in the ejs file.
+    // This includes the username, url to the profile pic in storage, etc.
+});
+
+// New Event Post Route
+Router.post('/events/new', (req, res) => {
+    // Create new event in the database from the form response data in req.body
+
+});
+
+// Delete Event Route
+Router.delete('/events/:id', (req, res) => {
+    // Delete event from the database and disassociate the event id from its owner and members
+});
+
 module.exports = router;
