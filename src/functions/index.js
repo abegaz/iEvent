d8c6487c37f18753def15268aeb2983066917e08
QuintinKerns <qakern8560@ung.edu>
2018-10-18 14:37:37 -0400

    Routing Works!

--- src/functions/index.js
+++ src/functions/index.js
@@ -12,23 +12,20 @@ const firebaseApp = firebase.initializeApp(functions.config().firebase);
 const app = express();
 
 // Views are our dynamic web pages that get rendered to HTML in our routes.js.
-app.set('views', '../views');
-
-// EJS is an easy-to-understand templating engine.
-// Templating engines let you put JS into HTML that is executed when the page is being rendered.
-app.use('../public', express.static(process.cwd() + '/public'));
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine('ejs', require('ejs').__express);
 
 // Require our routes.js file, which handles incoming HTTP requests and responses.
-const mainRoute = require('../routes/main.js');
-const eventsRoute = require('../routes/events.js');
-const userRoute = require('../routes/user.js');
+const mainRoute = require('./routes/main.js');
+const eventsRoute = require('./routes/events.js');
+const userRoute = require('./routes/user.js');
 
 // Anytime we get a request to '/', routes handles it.
 app.use('/', mainRoute);
-app.use('/events/', eventsRoute);
-app.use('/user/', userRoute);
+app.use('/events', eventsRoute);
+app.use('/user', userRoute);
 
 // MUST BE LAST
 // Exports our function.
-exports.app = functions.https.onRequest(app);
+exports.main = functions.https.onRequest(app);
