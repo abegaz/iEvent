061328914114b75c45a23dc2cf14db8c9060b2d6
QuintinKerns <qakern8560@ung.edu>
2018-10-23 13:45:37 -0400

    Refactored nodejs branch

--- functions/index.js
+++ functions/index.js
@@ -29,3 +29,10 @@ app.use('/user/', userRoute);
 // MUST BE LAST
 // Exports our function.
 exports.main = functions.https.onRequest(app);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
