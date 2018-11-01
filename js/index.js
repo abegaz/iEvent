ed3f5b9d7ac82d8879b7042fd8aab2609620e92c
QuintinKerns <qakern8560@ung.edu>
2018-11-01 18:40:34 -0400

    FullCalendar Started

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+  $('#calendar').fullCalendar({
+
+  });
+  $('#calendar').fullCalendar({
+    themeSystem: 'bootstrap4'
+  });
+});
+
+// Parse 
+function jsonToEvents(json){
+
+}
+
+function addEvent(name, startDate, endDate, href){
+
+}
