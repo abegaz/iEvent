f0fe68e15390c918378b900fa3789fccbdaa357b
kdmitc9478 <42586024+kdmitc9478@users.noreply.github.com>
2018-11-04 16:06:12 -0500

    Pull Request Fixes

---
+++ src/public_html/iEvent/application/controllers/iEvent-profile.php
@@ -0,0 +1,47 @@
+<head>
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>/css/iEvent-profile.css">
+</head>
+<body>
+<div class="row text-green">
+  <div class="leftcolumn">
+    <div class="card2 bg-gray">
+      <h2>Your Public Events</h2>     
+      <div><img class="cal-img" src="<?php echo site_url();?>/images/calender.jpg" alt="calender" style="height:400px;"></div>     
+    </div>
+	
+    <div class="card2 bg-gray">
+      <h2>Your Private Events</h2>     
+      <div><img class="cal-img" src="<?php echo site_url();?>/images/calender.jpg" alt="calender" style="height:400px;"></div>
+    </div>
+  </div>
+  
+  <div class="rightcolumn bg-gray">
+    <div class="card">
+      <h2>Profile</h2>
+      <div><img class="profile-img" src="<?php echo site_url();?>/images/profile-img.jpg" alt="profile-pic"></div>
+      <p>Name: blah</p>
+	  <p>Birthday: blah</p>
+	  <p>Employment: blah</p>
+	  <p>Stuff: blah</p>
+    </div>
+	
+    <div class="card bg-gray">
+      <h3>Events You May Know</h3>
+	  <p>Event 1</p>
+	  <p>Event 2</p>
+	  <p>Event 3</p>
+	  <p>Event 4</p>	  
+    </div>
+	
+    <div class="card bg-gray">
+      <h3>Contact</h3>
+      <p>Email: Stuff</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer bg-gray text-green">
+  <h2>Stuff | Stuff | Stuff | Stuff</h2>
+</div>
+
+</body>
